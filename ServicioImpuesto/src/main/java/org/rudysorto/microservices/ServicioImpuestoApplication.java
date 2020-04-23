0d71ea82e39f7e41b3ff8d50ec2d34c8b31c3f08
r <rudy.eli>
2020-04-23 15:02:51 -0600

    Init Commit -  only have a user story and description

---
+++ ServicioImpuesto/src/main/java/org/rudysorto/microservices/ServicioImpuestoApplication.java
@@ -0,0 +1,13 @@
+package org.rudysorto.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioImpuestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioImpuestoApplication.class, args);
+	}
+
+}
